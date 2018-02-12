4127ea7f68b8c140f1c39da2ed9a59e095011f3c
Ahmet Koylan <Ahmetburhan@gmail.com>
2018-02-12 02:24:02 -0800

    customer js updated

--- bamazon.sql
+++ bamazon.sql
@@ -5,14 +5,33 @@ CREATE DATABASE Bamazon;
 USE Bamazon;
 
 -- Create a table called 'products' which will contain the store inventory --
-CREATE TABLE products (
-	item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
-	product_name VARCHAR(30) NOT NULL,
-	department_name VARCHAR(20) NOT NULL,
-	price DECIMAL(10,2) NOT NULL,
-	stock_quantity INTEGER(11) NOT NULL,
-	PRIMARY KEY (item_id)
-);
+CREATE TABLE Products
+(
+	ItemID int NOT NULL,
+	ProductName varchar(999) NOT NULL,
+	DepartmentName varchar(50) NOT NULL,
+	Price decimal(10,2) NOT NULL,
+	StockQuantity int NOT NULL,
+	PRIMARY KEY (ItemID)
+)
 
 USE Bamazon;
-SELECT * FROM products
+SELECT *
+FROM Bamazon.Products;
+
+
+
+USE Bamazon;
+CREATE TABLE Departments
+(
+	DepartmentId int
+	AUTO_INCREMENT,
+PRIMARY KEY
+	(DepartmentId),
+DepartmentName varchar
+	(50) NOT NULL,
+OverHeadCosts DECIMAL
+	(11,2) NOT NULL,
+TotalSales DECIMAL
+	(11,2) NOT NULL);
+
